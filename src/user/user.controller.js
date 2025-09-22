73e438b44fa88235adc9c3575ea565ea28c633db
Javi <test@test.com>
2025-09-22 13:57:33 -0300

    Update src/user/user.controller.js

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,15 +1,72 @@
-exports.getUsers = (req, res) => {
-  res.status(200).json({ message: "TODO: Get users" });
+const { User } = require("./user.model");
+
+// Obtener todos los usuarios
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ status: "success", data: users });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: err.message });
+  }
+};
+// Obtener un usuario por ID
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
+    }
+    res.status(200).json({ status: "success", data: user });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: err.message });
+  }
 };
 
-exports.getUser = (req, res) => {
-  res.status(200).json({ message: "TODO: Get user" });
+exports.getUsersByDay = async (req, res) => {
+  try {
+    const { day } = req.params;
+
+    const users = await User.find({ availability: new RegExp(day, "i") });
+
+    res.status(200).json({ status: "success", data: users });
+  } catch (error) {
+    res.status(500).json({ status: "error", message: error.message });
+  }
 };
 
-exports.updateUser = (req, res) => {
-  res.status(200).json({ message: "TODO: Update user" });
+// Actualizar un usuario
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
+    }
+
+    Object.keys(req.body).forEach((key) => {
+      user[key] = req.body[key];
+    });
+
+    const updatedUser = await user.save();
+    res.status(200).json({ status: "success", data: updatedUser });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: err.message });
+  }
 };
 
-exports.deleteUser = (req, res) => {
-  res.status(200).json({ message: "TODO: Delete user" });
+exports.deleteUser = async (req, res) => {
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "User not found" });
+    }
+    res.status(200).json({ status: "success", message: "User deleted" });
+  } catch (err) {
+    res.status(500).json({ status: "error", message: err.message });
+  }
 };
