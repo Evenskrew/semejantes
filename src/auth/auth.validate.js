8beef120261df5d89d20ec91a507cb4bfa390ea1
EvenSkrew <evenskrew@gmail.com>
2025-10-31 15:00:35 -0300

    Updated auth.validate.js & added event.validate.js

--- src/auth/auth.validate.js
+++ src/auth/auth.validate.js
@@ -1,25 +1,35 @@
 const validateSignUpCoordinator = [
-    body("username").Lenght({min: 6}).withMessage("Invalid Username"),
-    body("email").isEmail().withMessage("Invalid email format"),
-    body("password").isLenght({min: 6}).withMessage("Password must be at least 6 characters long"),
-    body("phone").isLenght({min: 3}).withMessage("Phone must be at least 3 characters long"),
-    body("position").isLenght({min: 20}).withMessage("Position must be at least 20 characters long"),
+  body("username").isLength({ min: 6 }).withMessage("Invalid Username"),
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body("phone")
+    .isLength({ min: 3 })
+    .withMessage("Phone must be at least 3 characters long"),
+  body("position")
+    .isLength({ min: 20 })
+    .withMessage("Position must be at least 20 characters long"),
 ];
 
 const validateSignUpVolunteer = [
-    body("username").Lenght({min: 6}).withMessage("Invalid Username"),
-    body("email").isEmail().withMessage("Invalid email format"),
-    body("password").isLenght({min: 6}).withMessage("Password must be at least 6 characters long"),
-    body("phone").isLenght({min: 3}).withMessage("Phone must be at least 3 characters long"),
-    body("availability").isLenght({min: 20}).withMessage("Availability must be at least 20 characters long"),
-    body("speciality").isLenght({min: 25}).withMessage("Speciality must be at least 25 characters long"),
-    body("HoursContributed").isLenght({min:2}).withMessage("HoursContributed must be a number"),
-]; //Hours contributed como se hace para verificar que sea un numero?
+  body("username").isLength({ min: 6 }).withMessage("Invalid Username"),
+  body("email").isEmail().withMessage("Invalid email format"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+  body("phone")
+    .isLength({ min: 3 })
+    .withMessage("Phone must be at least 3 characters long"),
+  body("HoursContributed")
+    .isFloat({ min: 2 })
+    .withMessage("HoursContributed must be a number"),
+];
 
 const handleValidationErrors = (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }
-    next();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
 };
