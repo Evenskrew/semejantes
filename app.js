474ce046a22e1d05de59bc654e00bb0bc41d2456
EvenSkrew <evenskrew@gmail.com>
2025-11-29 04:37:11 -0300

    Updated App.js

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ require("dotenv").config({ quiet: true });
 
 const express = require("express");
 const connectMongoDB = require("./config/mongo-db");
-const { connectSQL } = require("./config/sql-db"); // Importar conexión SQL
+const { connectSQL } = require("./config/sql-db");
 const authRouter = require("./src/auth/auth.route");
 const usersRouter = require("./src/user/user.route");
 const eventsRouter = require("./src/event/event.route");
+const requestsRouter = require("./src/request/request.route");
+const donationsRouter = require("./src/donation/donation.route");
+const reportsRouter = require("./src/report/report.route");
+
 const cookieParser = require("cookie-parser");
 
 const app = express();
@@ -14,7 +18,6 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.static("public"));
 
-// Conectar ambas bases de datos
 if (process.env.NODE_ENV !== "test") {
   connectMongoDB();
   connectSQL();
@@ -23,10 +26,13 @@ if (process.env.NODE_ENV !== "test") {
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/events", eventsRouter);
+app.use("/api/requests", requestsRouter);
+app.use("/api/donations", donationsRouter);
+app.use("/api/reports", reportsRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({
-    message: "Backend Híbrido (SQL + NoSQL) funcionando :D",
+    message: "Semejantes API completa funcionando (SQL + NoSQL)",
   });
 });
 
